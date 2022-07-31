b8fdde133e5b53598d9d496e40c728da4f6569af
Siddique3999 <110351375+Siddique3999@users.noreply.github.com>
2022-07-31 23:58:53 +0500

    Create constrain.php

---
+++ constrains/constrain.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+//Create constrains
+define('SETURL' , 'http://localhost/QauFoodFinder/');
+define('LOCALHOST' , 'localhost');
+define('DB_USERNAME' , 'root');
+define('DB_PASSWORD' , '');
+define('DB_NAME' , 'qaufoodfinder');
+
+$conn = mysqli_connect(LOCALHOST, DB_USERNAME , DB_PASSWORD) or die(mysqli_error());
+$db_select = mysqli_select_db($conn , DB_NAME) or die(mysqli_error());
+?>
