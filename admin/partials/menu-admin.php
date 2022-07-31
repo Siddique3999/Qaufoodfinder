f2159009580fa798494c3b32df3fdf77fbbea0a3
Siddique3999 <110351375+Siddique3999@users.noreply.github.com>
2022-07-31 23:55:48 +0500

    Add files via upload

---
+++ admin/partials/menu-admin.php
@@ -0,0 +1,25 @@
+<?php include('../constrains/constrain.php')?>
+<?php include('../admin/login-check.php') ?>
+<html>
+    <head>
+    <title> Qau Food Finder </title>
+    <link rel = "stylesheet" href ="..\css\admin.css" >
+
+    </head>
+
+ <body>
+
+     <!-- menu starts here  -->
+     <div class = "Menu">
+         <div class = "wrapper text-align">
+          <ul>
+              <li><a href="index.php">Home</a></li>
+              <li><a href="manage-manager.php">Managers</a></li>
+              <!-- <li><a href="manage-category.php">Hotels</a></li>
+              <li><a href="manage-food.php">Food</a></li>
+              <li><a href="manage-order.php">Order</a></li> -->
+              <li><a href="logout.php">Logout</a></li>
+          </ul>
+         </div>
+     </div>
+     <!-- menu ends here  -->
